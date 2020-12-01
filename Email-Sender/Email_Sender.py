545597cb17a10729a9f5ec1c37d0672bc3e606d2
saadishahid <39744515+saadishahid@users.noreply.github.com>
2020-11-30 16:00:53 -0800

    Create Email_Sender.py

---
+++ Email-Sender/Email_Sender.py
@@ -0,0 +1,23 @@
+import os
+import smtplib
+
+userid = os.environ.get('Gmail_Python_User')
+password = os.environ.get('Gmail_Python_Password')
+
+#with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+with smtplib.SMTP('localhost', 1025) as smtp:
+
+    # smtp.ehlo()
+    # smtp.starttls()
+    # smtp.ehlo()
+
+    #smtp.login(userid, password)
+
+    subject = 'Foo' #enter subject of email
+    body = 'Bar'    #enter body of email
+
+    msg = f'Subject:{subject}\n\n{body}'
+   # smtp.sendmail(userid, '<enter email of adressee', msg)
+
+    
+
